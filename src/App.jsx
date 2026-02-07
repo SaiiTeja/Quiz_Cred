a0cbb6c3efe653a8a91a8a6dd6ae06c2a14daaf0
piyushin045 <piyushin045@gmail.com>
2026-02-08 02:11:13 +0530

    add certification.jsx patner.jsx customCard.jsx certification.css and partner.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import Home from "./pages/Home.jsx";
 import './App.css'
+import Certifications from './pages/Certifications.jsx';
+import Partners from './pages/Partener.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
     
-      <Home/>
+      <Certifications/>
+      <Partners/>
     </>
   )
 }
